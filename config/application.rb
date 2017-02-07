188dd834ea94f3c87c7b5da400845c3c1d27378c
Erik Reyna <erik.reyna@washpost.com>
2017-02-07 16:45:35 -0500

    whitelist ips

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,10 @@ end
 
 module Klaxon
   class Application < Rails::Application
+    # Docker ips change when using ALB setup
+    config.web_console.whitelisted_ips = ENV['DOCKER_HOST_IP']
+    
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
