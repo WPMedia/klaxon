e01b61c21c42d31fe2aab5fd6e0fe7ede1ce686c
Katlyn Alo <26312638+Kat-Alo@users.noreply.github.com>
2023-01-13 18:22:40 -0700

    prod config needed for emailing with SES

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.action_mailer.delivery_method = :postmark
+  config.action_mailer.delivery_method = :smtp
   # config.action_mailer.postmark_settings = { :api_token => ENV['POSTMARK_API_TOKEN'] }
 
   provider  = (ENV["SMTP_PROVIDER"] || "SENDGRID").to_s
