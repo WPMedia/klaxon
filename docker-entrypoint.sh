9bba85ddfa2d2856f133eced4f2a939665201163
Aaron Brezel <aaron.brezel@washpost.com>
2023-02-02 15:14:22 -0500

    fix/precompile assets

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,10 @@
 echo "Calling script to set up server cronjobs"
 /bin/bash scripts/cron_setup.sh
 
+echo "precompiled %%%%%%%%%%%%%%%%%%%%"
+bundle exec rails assets:precompile
+
 # Launch the main container command passed as arguments.
 exec "$@"
+
+
