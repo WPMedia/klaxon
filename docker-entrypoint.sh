4eb051700393bd574edc5eec68ca7bfc3e2641ef
Aaron Brezel <aaron.brezel@washpost.com>
2023-03-27 09:46:46 -0400

    moving initial rake check

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,13 +4,13 @@ if [[ "$USE_CRON" == "true" ]]; then
     # Add crontab jobs
     echo "Calling script to set up server cronjobs"
     /bin/bash scripts/cron_setup.sh
+    
+    # Initialize app with initial diff check on watched pages
+    echo "Initial check on watched pages" 
+    bundle exec rake check:all
 else
     echo "Skipping cron setup"
 fi
-
-# Initialize app with initial diff check on watched pages
-echo "Initial check on watched pages" 
-bundle exec rake check:all
 
 # Compile static assets before launching server
 bundle exec rails assets:precompile
